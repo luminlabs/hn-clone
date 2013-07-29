6c13e486459da39c0bac6a3cdb620cc021e0f5ed
Jacqueline Herrlin <j.m.herrlin@gmail.com>
2013-07-29 16:29:26 -0700

    green

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,16 @@
+class LinksController < ApplicationController
+
+	def index
+		@links = Link.all
+	end
+
+	def create
+		@link = Link.new(params[:link])
+		if @link.save
+			render :show
+		else
+			render :new
+		end
+	end
+
+end
